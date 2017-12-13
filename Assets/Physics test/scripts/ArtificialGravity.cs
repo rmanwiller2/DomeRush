47edd45380873e702058ed4fc883e8f4657abebf
Ryan Manwiller <rmanwiller2@gmail.com>
2017-12-13 10:06:29 -0900

    physics stuff

---
+++ Assets/Physics test/scripts/ArtificialGravity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtificialGravity : MonoBehaviour {
+    public float attenuation = .5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+         GetComponent<Rigidbody>().AddForce((GetVector()*attenuation),ForceMode.Force);
+	}
+
+    private Vector3 GetVector()
+    {
+        Vector3 heading =   transform.position - new Vector3(0,0,0); // get the vector between the center and this object
+        float distance = heading.magnitude;
+        Vector3 direction = heading / distance; // This is now the normalized direction.
+        print(direction);
+        return direction;
+
+    }
+}
---
+++ Assets/Physics test/scripts/CharController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+    public float speed = .5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        GetComponent<Rigidbody>().AddForce(movement*speed, ForceMode.Impulse);
+	}
+}
