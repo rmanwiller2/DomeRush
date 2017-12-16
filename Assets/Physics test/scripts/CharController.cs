8356c056458027827aba8ebdc5c539add641ed66
Ryan Manwiller <rmanwiller2@gmail.com>
2017-12-16 02:30:35 -0900

    Physics Update

--- Assets/Physics test/scripts/CharController.cs
+++ Assets/Physics test/scripts/CharController.cs
@@ -7,6 +7,10 @@ public class CharController : MonoBehaviour {
     public float turnSpeed = .5f;
     private Rigidbody rb;
     public Vector2 movement;
+
+    [Header("Auto Upright")]
+    private RaycastHit hit;
+
     [Header("DEBUG")]
     public MeshRenderer mr;
 
@@ -22,6 +26,31 @@ public class CharController : MonoBehaviour {
         rb.AddTorque( transform.up * movement.x * turnSpeed);
         rb.AddForce(transform.forward * movement.y * speed, ForceMode.Impulse);
 	}
+
+    void FlipOver()
+    {
+
+        Vector3 castPos = new Vector3(transform.position.x, transform.position.y - .25f, transform.position.z);
+        if (Physics.Raycast(castPos, -transform.up, out hit))
+        {
+            
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //Here be DEBUG, all ye beware!
     private void OnCollisionEnter(Collision collision)
