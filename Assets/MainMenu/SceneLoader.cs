556f1b0c1f555526a9f05a92822e2ecf7c093dfd
Ryan Manwiller <rmanwiller2@gmail.com>
2017-12-19 19:51:30 -0900

    Final Submission

--- Assets/MainMenu/SceneLoader.cs
+++ Assets/MainMenu/SceneLoader.cs
@@ -9,6 +9,6 @@ public class SceneLoader : MonoBehaviour
     void Start()
     {
         // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene("Final Game Scene", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Final Game Scene", LoadSceneMode.Single);
     }
 }
