850a8fff8b4f0bc63ec976d1c7e0702e6f02a445
Ryan Manwiller <rmanwiller2@gmail.com>
2017-12-19 15:51:09 -0900

    Finalizing everything
    Removed a few test scenes and combined everything into a nice folder structure which should allow for us to cleanly turn this in. I am not sure what assets you need for the main menu. additionally we need to connect the main menu with the game scene so it starts the game.

---
+++ Assets/Final Game/Scripts/GoalController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour {
+    public GameObject ScriptCache;
+    public int points = 100;
+    [Header("Effects")]
+    public AudioClip goalSound;
+    private AudioSource SFX;
+    private ParticleSystem particles;
+    private ScoreScript score;
+    private ResetScript reset;
+    private bool inGoal;
+    // Use this for initialization
+    void Start () {
+        particles = GetComponent<ParticleSystem>();
+        particles.Stop();
+        SFX = GetComponent<AudioSource>();
+        reset = ScriptCache.GetComponent<ResetScript>();
+        score = ScriptCache.GetComponent<ScoreScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            
+            if (!inGoal) {
+                particles.Play();
+                SFX.PlayOneShot(goalSound, .8f); 
+                score.AddPoints(points);
+                reset.NewRound();
+                inGoal = true;
+            }
+        }
+    }
+}
---
+++ Assets/Final Game/Scripts/ScoreScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+    public int score;
+    [Header("GUI Connections")]
+    public Text scoreText;
+	// Use this for initialization
+	void Start () {
+        scoreText.text = "POINTS: " + score;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+    }
+    public void AddPoints(int points) {
+        score += points;
+        scoreText.text = "POINTS: " + score;
+    }
+}
